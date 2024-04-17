8147786c76cb4f33db0e5a26b1212c24d1a79c74
Rahtu Anom <100014294+rahtuanom@users.noreply.github.com>
2024-04-17 14:30:50 +0800

    Update db.js

--- db.js
+++ db.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2/promise');
 
 // Konfigurasi koneksi ke database
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root', // Ganti dengan username Anda
-  password: '', // Ganti dengan password Anda
-  database: 'db_ipk' // Ganti dengan nama database Anda
+  MYSQLHOST: 'monorail.proxy.rlwy.net',
+  MYSQLUSER: 'root', // Ganti dengan username Anda
+  MYSQLPASSWORD: 'LxBhwwmwlJpDAJJBEzfoqFhEBKPtIqTe', // Ganti dengan password Anda
+  MYSQLDATABASE: 'railway' // Ganti dengan nama database Anda
 });
 
 module.exports = connection;
