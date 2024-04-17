9dfc60ce3a1f343ac29185e026ffb56dbb72e8dc
Rahtu Anom <100014294+rahtuanom@users.noreply.github.com>
2024-04-17 14:31:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors'); // Import modul cors
 const mahasiswaRoutes = require('./routes/mahasiswa');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.MYSQLPORT || 51651;
 
 // app.use(cors()); 
 app.use(cors({
