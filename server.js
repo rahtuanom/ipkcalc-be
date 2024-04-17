401bccd17baecc2c6955fbcad3a8ffcebc84ca5d
Rahtu Anom <100014294+rahtuanom@users.noreply.github.com>
2024-04-17 20:23:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,70 +2,24 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const db = require('db'); // Sesuaikan dengan lokasi file database Anda
+const cors = require('cors'); // Import modul cors
+const mahasiswaRoutes = require('./routes/mahasiswa');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.MYSQLPORT || 51651;
 
-// Middleware
+app.use((req, res, next) => {
+    res.removeHeader('Permissions-Policy');
+    next();
+});
+
+app.use(cors()); 
+app.use(cors({
+  origin: 'https://rahtuanom.github.io/' // Atur domain atau port yang diizinkan
+}));
 app.use(bodyParser.json());
+app.use('/mahasiswa', mahasiswaRoutes);
 
-// Endpoint untuk mendapatkan data mahasiswa berdasarkan NIM
-app.get('/mahasiswa/:nim', (req, res) => {
-    const { nim } = req.params;
-    const query = 'SELECT * FROM mahasiswa WHERE nim = ?';
-    
-    db.query(query, [nim], (err, result) => {
-        if (err) {
-            console.error('Error executing query:', err);
-            res.status(500).json({ error: 'Internal server error' });
-        } else {
-            if (result.length > 0) {
-                res.json(result[0]);
-            } else {
-                res.status(404).json({ error: 'Mahasiswa tidak ditemukan' });
-            }
-        }
-    });
-});
-
-// Endpoint untuk menghitung IPK berdasarkan NIM
-app.get('/mahasiswa/:nim/ipk', (req, res) => {
-    const { nim } = req.params;
-    const query = `
-        SELECT 
-            NIM,
-            SUM(CASE 
-                WHEN krs.nilai >= 80 THEN 4 * mata_kuliah.sks
-                WHEN krs.nilai >= 60 THEN 3 * mata_kuliah.sks
-                WHEN krs.nilai >= 40 THEN 2 * mata_kuliah.sks
-                ELSE 0
-            END) / SUM(sks) AS IPK
-        FROM 
-            krs
-        JOIN 
-            mata_kuliah ON krs.id_mk = mata_kuliah.id_mk
-        WHERE 
-            NIM = ?
-        GROUP BY 
-            NIM;
-    `;
-
-    db.query(query, [nim], (err, result) => {
-        if (err) {
-            console.error('Error executing query:', err);
-            res.status(500).json({ error: 'Internal server error' });
-        } else {
-            if (result.length > 0) {
-                res.json(result[0]);
-            } else {
-                res.status(404).json({ error: 'Mahasiswa tidak ditemukan' });
-            }
-        }
-    });
-});
-
-// Jalankan server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(Server is running on port ${PORT});
 });
