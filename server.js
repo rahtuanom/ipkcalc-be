bde4b9f97c10e570c8bd807a837a985f7ffd4b8c
Rahtu Anom <100014294+rahtuanom@users.noreply.github.com>
2024-04-17 21:06:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -16,5 +16,5 @@ app.use(bodyParser.json());
 app.use('/mahasiswa', mahasiswaRoutes);
 
 app.listen(PORT, () => {
-  console.log(Server is running on port ${PORT});
+  console.log(`Server is running on port ${PORT}`);
 });
